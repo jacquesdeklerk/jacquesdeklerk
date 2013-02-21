3e0cffd3d168b5aa6e315e9a73065c960b29395e
Jacques de Klerk <jacques.de.klerk@gmail.com>
2013-02-21 08:35:33 +0200

    Added susy grids and content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
+	$('.project-head, .images').click(function(e) {
+	    e.preventDefault();
+		$(this).parents('article').toggleClass('inactive').toggleClass('expanded');
+	});
+	
+	
+	//add expand symbol to headings
+	$('.project-heading').prepend('<a href="#" class="expander"></a>');
+	
+	
+	$('.project').hover(
+      function() {
+        $(this).toggleClass('focused');  
+      }
+    );
 	
 });
